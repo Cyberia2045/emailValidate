cb94ab515ed8c3cdd92fb1f7b7bf64acddf6407f
Cyberia 2045 <sagansociety86@gmail.com>
2017-04-28 12:03:28 -0400

    Created a function that allows client side email validity

---
+++ scripts.js
@@ -0,0 +1,23 @@
+function testEmail() {
+	email = email.toLowerCase();
+
+	if (email.indexOf("@") < 0) {
+		return false;
+	}
+
+	var beforeAt = email.split("@")[0];
+	var afterAt = email.split("@")[1];
+
+	if (beforeAt === "" || afterAt.indexOf(".") < 0 || beforeAt.indexOf(".") > 0) {
+		return false;
+	}
+
+	AfterAt = AfterAt.split(".");
+
+	if (afterAt.length > 2 || afterAt[1] === "" || afterAt[0] === "") {
+		return false;
+	}
+
+	return true;
+
+}
